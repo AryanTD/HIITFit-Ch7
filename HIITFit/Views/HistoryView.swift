fe5b432b7dc123a0cf7a074fe11377d599520150
AryanTD <aryantandon285@gmail.com>
2022-02-28 14:27:48 -0500

    Initial Commit

---
+++ HIITFit/Views/HistoryView.swift
@@ -0,0 +1,45 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in 
+import SwiftUI
+
+struct HistoryView: View {
+  @Binding var showHistory: Bool
+  @EnvironmentObject var history: HistoryStore
+
+  var body: some View {
+    ZStack(alignment: .topTrailing) {
+      Button(action: { showHistory.toggle() }) {
+        Image(systemName: "xmark.circle")
+      }
+      .font(.title)
+      .padding(.trailing)
+      VStack {
+        Text("History")
+          .font(.title)
+          .padding()
+        Form {
+          ForEach(history.exerciseDays) { day in
+            Section(
+              header:
+                Text(day.date.formatted(as: "MMM d"))
+                .font(.headline)) {
+              ForEach(day.exercises, id: \.self) { exercise in
+                Text(exercise)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+  static var previews: some View {
+    HistoryView(showHistory: .constant(true))
+      .environmentObject(HistoryStore())
+  }
+}
